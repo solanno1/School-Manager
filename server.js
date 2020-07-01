2449fa7e8ac5475b176463e0740facfea3713ad8
solanno1 <solano.oliveira@fatec.sp.gov.br>
2020-07-01 08:27:11 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const server = express()
+
+const routes = require("./routes")
+server.use(express.urlencoded({ extended: true }))
+
+server.use(express.static('public'))
+
+server.use(routes)
+
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
